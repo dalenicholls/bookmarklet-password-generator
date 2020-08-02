504604cfbdb3cf2efa3102b92a808ca2f1674e83
Dale Nicholls <dlnchlls@gmail.com>
2020-08-02 22:56:01 +0100

    Update bookmarklet-password-generator.js

--- bookmarklet-password-generator.js
+++ bookmarklet-password-generator.js
@@ -6,16 +6,5 @@ javascript: (function() {
   		pwd += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   
-  var password = prompt("Suggested password:", pwd);
-})();
-
-javascript: (function() {
-  var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*=-_';
-  
-  var pwd = '';
-  for(var i = 0; i<12; i++){
-  		pwd += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  
   prompt("Suggested password:", pwd);
 })();
