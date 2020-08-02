3205c07c7bd7e0eb41772d048a86cba66713f473
Dale Nicholls <dlnchlls@gmail.com>
2020-08-02 22:55:36 +0100

    prompt variable name removed

--- bookmarklet-password-generator.js
+++ bookmarklet-password-generator.js
@@ -6,5 +6,16 @@ javascript: (function() {
   		pwd += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   
-  var person = prompt("Suggested password:", pwd);
+  var password = prompt("Suggested password:", pwd);
+})();
+
+javascript: (function() {
+  var characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*=-_';
+  
+  var pwd = '';
+  for(var i = 0; i<12; i++){
+  		pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  
+  prompt("Suggested password:", pwd);
 })();
